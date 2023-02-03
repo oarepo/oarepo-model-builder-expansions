c926ed7f676d1d25947f15e686c3b06a4bff588a
Ronald Krist <ronald.krist@cesnet.cz>
2023-02-03 13:52:45 +0100

    initial commit

---
+++ run_tests.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+MODEL_WITH_EXPANDABLE_FIELDS="model_document"
+MODEL_REFERENCED="model_file"
+VENV=".model_venv"
+export OPENSEARCH_PORT=9400
+python3 -m venv $VENV
+. $VENV/bin/activate
+pip install -U setuptools pip wheel
+#cd $(dirname $0)/..
+for MODEL in $MODEL_WITH_EXPANDABLE_FIELDS $MODEL_REFERENCED
+do
+  if test -d ./tests/$MODEL; then
+    rm -rf ./tests/$MODEL
+  fi
+  oarepo-compile-model ./tests/$MODEL.yaml --output-directory ./tests/$MODEL -vvv
+  pip install "./tests/$MODEL[tests]"
+done
+pytest tests/test_models.py
